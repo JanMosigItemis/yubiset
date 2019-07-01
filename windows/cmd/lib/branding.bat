a1ab6789cc09483ce555e622ca925f846007678d
Jan Mosig <jan.mosig@itemis.de>
2019-07-01 14:36:51 +0200

    Created customized itemis branding branch.

--- windows/cmd/lib/branding.bat
+++ windows/cmd/lib/branding.bat
@@ -5,10 +5,8 @@ REM
 REM Arg 1: User name
 REM
 
-set branded_user_name=%~1
-REM Be aware: GPG does not support arbitrary charaters in key comments. Especially parantheses '(' and ')' will cause problems.
-REM On Windows some additional characters may cause trouble, e. g. * ? & or %. Don't use them.
-set branded_user_comment=
+set branded_user_name=%~1 (itemis AG)
+set branded_user_comment=Vocational OpenPGP key of itemis AG's %~1
 
 REM What follows is a trick to get the variables into the context of the calling script (which should be a local context as well) without polluting the global env.
 REM See https://stackoverflow.com/a/16167938
