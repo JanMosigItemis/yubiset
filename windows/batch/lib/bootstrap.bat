4ddb218e457991cfa5785b888d698883a711aaeb
Jan Mosig <jan.mosig@itemis.de>
2019-07-04 20:54:19 +0200

    Added doc on download verification

--- windows/batch/lib/bootstrap.bat
+++ windows/batch/lib/bootstrap.bat
@@ -6,7 +6,8 @@ REM Arg 1: Full path to caller script
 REM Arg 2: Full path to yubiset windows script folder
 REM
 
-set yubiset_version=0.4.1.itemis
+
+set yubiset_version=0.4.2.itemis
 set me=%~1
 set root_folder=%~2..\..
 set error_prefix=ERROR
