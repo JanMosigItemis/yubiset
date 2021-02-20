e15e133542f0a05e61fb99520723d9f6c836a220
Jan Mosig <jan.mosig@itemis.de>
2021-02-20 15:48:15 +0100

    Renewed signing key

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -11,14 +11,18 @@ declare -r secring_auto="${custom_gpg_home}/secring.auto"
 declare -r pubring_auto="${custom_gpg_home}/pubring.auto"
 
 echo
+echo Importing required public keys..
+{ gpg --home "${custom_gpg_home}" --import "${custom_gpg_home}/37F0780907ABEF78.pub.asc" ; } || { end_with_error "Could not import public key into gpg." ; }
+{ gpg --home "${custom_gpg_home}" --import "${pubring_auto}" ; } || { end_with_error "Could not import public key into gpg." ; }
+
+echo
 echo "Decrypting secret gpg keyring.."
-{ echo $super_secret_password | gpg --passphrase-fd 0 "${secring_auto}".gpg ; } || { end_with_error "Failed to decrypt secret gpg keyring." ; }
+{ echo $super_secret_password | gpg --home "${custom_gpg_home}" --passphrase-fd 0 "${secring_auto}".gpg ; } || { end_with_error "Failed to decrypt secret gpg keyring." ; }
 echo Success!
 
 echo
-echo Importing keyrings..
+echo Importing required private keys..
 { gpg --home "${custom_gpg_home}" --import "${secring_auto}" ; } || { end_with_error "Could not import secret keyring into gpg." ; }
-{ gpg --home "${custom_gpg_home}" --import "${pubring_auto}" ; } || { end_with_error "Could not import public keyring into gpg." ; }
 echo Success!
 
 echo
